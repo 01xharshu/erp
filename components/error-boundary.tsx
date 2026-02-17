82573b3cce97ad5707764230ad303645760d7137
Harsh Mishra <108367606+01xharshu@users.noreply.github.com>
2026-02-17 06:38:42 +0000

    commit

--- components/error-boundary.tsx
+++ components/error-boundary.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import { AlertCircle } from "lucide-react";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 
